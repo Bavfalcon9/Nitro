f2010a663f6ad65f8658f6802c605ffc1379f20e
Olybear9 <olybear9@gmail.com>
2020-07-03 16:16:57 -0500

    Even better logging

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,4 +1,4 @@
-import Colors from './Colors.ts';
+import * as fmtColors from "https://deno.land/std/fmt/colors.ts";
 
 class Logger {
     public static DEBUG_ENABLED = false;
@@ -11,24 +11,24 @@ class Logger {
     public debug(msg: string): void {
         if (!Logger.DEBUG_ENABLED) return;
         const named: string = `[${this.name}/DEBUG]: `;
-        this.colorLog(named + msg, Colors.Dim);
+        this.colorLog(named + msg, 0x969696);
         return;
     }
 
     public warn(msg: string): void {
         const named: string = `[${this.name}/WARN]: `;
-        this.colorLog(named + msg, Colors.FgRed);
+        this.colorLog(named + msg, 0xff6a45);
         return;
     }
 
     public notice(msg: string): void {
         const named: string = `[${this.name}/NOTICE]: `;
-        this.colorLog(named + msg, Colors.FgBlue);
+        this.colorLog(named + msg, 0x41f7fa);
         return;
     }
 
-    public colorLog(msg: string, color: string): void {
-        console.log(color + msg);
+    public colorLog(msg: string, color: number): void {
+        console.log(fmtColors.rgb24(msg, color));
         return;
     }
 }

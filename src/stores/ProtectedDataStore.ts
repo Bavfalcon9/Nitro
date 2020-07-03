2d8f97ebcfd9339884df93d6bb49fe18cb1ea80c
Olybear9 <olybear9@gmail.com>
2020-07-03 13:53:40 -0500

    Fix Syntax

--- src/stores/ProtectedDataStore.ts
+++ src/stores/ProtectedDataStore.ts
@@ -2,15 +2,15 @@ class ProtectedDataStore {
     public static token?: string;
 
     hash(key: string, value: any): string {
-     if(typeof value == 'object') {
-        JSON.parse(value);
-     }
-     let protectedStream: string = '';
-     for (let i of value) {
-      let cl = value.charCodeAt(i);
-      protectedStream += cl + (this.startTime - this.randomInc);// get the charCode
-     }
-     return protectedStream;
+        if(typeof value == 'object') {
+            JSON.parse(value);
+        }
+        let protectedStream: string = '';
+        for (let i of value) {
+            let cl = value.charCodeAt(i);
+            //protectedStream += cl + (this.startTime - this.randomInc);// get the charCode
+        }
+        return protectedStream;
     }
 }
 export default ProtectedDataStore;

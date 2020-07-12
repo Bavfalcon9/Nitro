b292fa2e7735b17d23c0ba1bf4d2b5eb7e58eaed
Bavfalcon9 <olybear9@gmail.com>
2020-07-12 17:55:54 -0500

    Chore: Fix this

--- src/structures/Base.ts
+++ src/structures/Base.ts
@@ -1,15 +1,11 @@
 class Base {
-    private _id: string;
+    public id: string;
     constructor (id: string) {
-        this._id = id;
-    }
-
-    get id() {
-        return this._id;
+        this.id = id;
     }
 
     get createdAt() {
-        return Math.floor(parseInt(this._id) / 4194304) + 1420070400000;
+        return Math.floor(parseInt(this.id) / 4194304) + 1420070400000;
     }
 }
 export default Base;

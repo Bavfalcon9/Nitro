3592dc6bc4ccb753caed89b9056815f592a3cc88
CadetMercury615 <60723938+CadetMercury615@users.noreply.github.com>
2020-08-17 17:56:49 -0400

    Chore: Add Role class

---
+++ src/structures/guild/Role.ts
@@ -0,0 +1,19 @@
+import Base from "../Base.ts";
+
+class Role extends Base {
+    public name: string;
+    public permissions: any;
+    public color: string;
+    public hoist: boolean;
+    public mentionable: boolean;
+
+    constructor(data: any) {
+        super(data.id)
+        this.name = data.name;
+        this.permissions = data.permissions;
+        this.color = data.color;
+        this.hoist = data.hoist;
+        this.mentionable = data.mentionable;
+    }
+}
+export default Role;

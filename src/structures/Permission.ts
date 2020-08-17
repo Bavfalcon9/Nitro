dde5d6e66279a316236a5f5e9c3b2312e8f24386
CadetMercury615 <60723938+CadetMercury615@users.noreply.github.com>
2020-08-17 17:57:45 -0400

    Chore: Add Permission class (Don't use)

---
+++ src/structures/Permission.ts
@@ -0,0 +1,41 @@
+import Base from "./Base.ts";
+import { Permissions } from "./Permissions.ts";
+
+class Permission extends Base {
+    public allow: number;
+    public deny: number;
+    public _json: object = {};
+    constructor(allow: number, deny: number = 0) {
+        super('')
+        this.allow = allow;
+        this.deny = deny;
+    }
+    get json() {
+        if(!this._json) {
+            this._json = {} as any;
+            for(const perm of Object.keys(Permissions)) {
+                if(!perm.startsWith("all")) {
+                    //@ts-ignore
+                    if(this.allow & Permissions[perm]) {
+                        //@ts-ignore
+                        this._json[perm] = true;
+                        //@ts-ignore
+                    } else if(this.deny & Permissions[perm]) {
+                        //@ts-ignore
+                        this._json[perm] = false;
+                    }
+                }
+            }
+        }
+        return this._json;
+    }
+
+    has(permission: string) {
+        //@ts-ignore
+        return !!(this.allow & Permissions[permission]);
+    }
+
+    toJSON(props = []) {
+    }
+}
+export default Permission

6ba2492a0f198983eca1a74daaa8c5cb7e7608a7
Cqdet <60723938+Cqdet@users.noreply.github.com>
2020-09-09 08:23:26 -0400

    Create GuildChannel.ts, John fix caching :o

---
+++ src/structures/channel/GuildChannel.ts
@@ -0,0 +1,27 @@
+import Guild from "../guild/Guild.ts";
+import PermissionOverwite from "../PermissionOverwrite.ts";
+import Channel from "./Channel.ts";
+
+class GuildChannel extends Channel {
+    public guild: Guild | undefined;
+    public id: string
+    public name: string;
+    public type: number;
+    public nsfw: boolean;
+    public position: string
+    public parentID: string;
+    public permissionOverwrites: PermissionOverwite[];
+
+    constructor(data: any, guild?: Guild) {
+        super(data);
+        this.id = data.id
+        this.guild = guild;
+        this.name = data.name;
+        this.type = data.type;
+        this.nsfw = data.nsfw;
+        this.position = data.position;
+        this.parentID = data.parent_id;
+        this.permissionOverwrites = data.permission_overwrites ? data.permission_overwrites.map((p: any) => new PermissionOverwite(p)) : [];
+    }
+}
+export default GuildChannel

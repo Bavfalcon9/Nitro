e1cba025ef6837c1cf4907a47bdae9bdb99c5733
Cqdet <60723938+Cqdet@users.noreply.github.com>
2020-11-10 17:34:46 -0500

    fix(modules): Add exported classes

--- mod.ts
+++ mod.ts
@@ -1,1 +1,17 @@
 export { default as Client } from './src/Client.ts';
+
+export { default as User } from './src/structures/User.ts';
+export { default as ClientUser } from './src/structures/ClientUser.ts';
+export { default as Message } from './src/structures/Message.ts';
+export { default as Channel } from './src/structures/channels/Channel.ts';
+export { default as GuildChannel } from './src/structures/channels/GuildChannel.ts';
+export { default as TextChannel } from './src/structures/channels/TextChannel.ts';
+export { default as VoiceChannel } from './src/structures/channels/VoiceChannel.ts';
+export { default as CategoryChannel } from './src/structures/channels/CategoryChannel.ts';
+export { default as Guild } from './src/structures/guild/Guild.ts';
+export { default as Member } from './src/structures/guild/Member.ts';
+export { default as Role } from './src/structures/guild/Role.ts';
+export { default as Emoji } from './src/structures/guild/Emoji.ts';
+
+export { default as SimpleEmbed } from './src/util/Embed.ts';
+export { default as Collection } from './src/util/Collection.ts';

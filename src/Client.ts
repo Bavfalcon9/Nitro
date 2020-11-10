c464cc625ec3c29ccce930e5d8be800d354af9fe
Cqdet <cadetmercury615@gmail.com>
2020-11-09 22:34:37 -0500

    chore(core): Add major gateway features

--- src/Client.ts
+++ src/Client.ts
@@ -1,9 +1,60 @@
 import { EventEmitter } from '../deps.ts';
+import CacheOptions from './cache/CacheOptions.ts';
+import Intents from './constants/Intents.ts';
+import Shard from './gateway/Shard.ts';
 import ProtectedDataStore from './util/ProtectedDataStore.ts';
+
+interface ClientOptions {
+	cacheOptions: CacheOptions;
+	intents: (keyof typeof Intents)[] | number; // Not sure why this doesn't work
+}
+
+const DefaultOptions: ClientOptions = {
+	cacheOptions: {
+		guilds: {
+			$enabled: true,
+			$max: Infinity,
+		},
+		channels: {
+			$enabled: true,
+			$max: Infinity,
+		},
+		users: {
+			$enabled: true,
+			$max: Infinity,
+		},
+		messages: {
+			$enabled: false,
+			$max: 0,
+		},
+	},
+	intents: ['guilds', 'guildMembers', 'guildMessages'],
+};
+
 export default class Client extends EventEmitter {
-	public protectedDataStore: ProtectedDataStore;
-	constructor(token: string, opt: any) {
+	// public protectedDataStore: ProtectedDataStore; I WILL WORK ON THIS, it's unstable
+	private token: string;
+	public shard: Shard;
+	public opt: ClientOptions;
+	public constructor(token: string, opt: ClientOptions = DefaultOptions) {
 		super();
-		this.protectedDataStore = new ProtectedDataStore();
+		// this.protectedDataStore = new ProtectedDataStore();
+		// this.protectedDataStore.set('TOKEN', token);
+		this.token = token;
+		this.shard = new Shard(this, token);
+		let intents = 0;
+		if (typeof opt.intents !== 'number') {
+			for (const intent of opt.intents) {
+				if (Intents[intent]) {
+					intents |= Intents[intent];
+				}
+			}
+			opt.intents = intents;
+		}
+		this.opt = opt;
+	}
+
+	public connect() {
+		this.shard.connect();
 	}
 }
--- src/gateway/Shard.ts
+++ src/gateway/Shard.ts
@@ -1,2 +1,75 @@
-import { EventEmitter } from '../../deps.ts';
-export default class Shard extends EventEmitter {}
+import { EventEmitter, Buffer } from '../../deps.ts';
+import Client from '../Client.ts';
+import Endpoints from '../constants/Endpoints.ts';
+import OPCodes from '../constants/OPCodes.ts';
+export default class Shard extends EventEmitter {
+	private client: Client;
+	private token: string;
+	private ws: WebSocket;
+
+	private heartbeatInterval: number = Infinity;
+	public constructor(client: Client, token: string) {
+		super();
+		this.client = client;
+		this.token = token;
+		this.ws = new WebSocket(Endpoints.GATEWAY_URL);
+	}
+
+	public connect() {
+		this.ws.onmessage = (ev: MessageEvent) => {
+			const data = JSON.parse(ev.data);
+
+			switch (data.op) {
+				case OPCodes.HELLO: {
+					this.heartbeat(data.d.heartbeat_interval);
+					this.identify();
+					break;
+				}
+				case OPCodes.EVENT: {
+					const func =
+						'on' +
+						data.t
+							.toLowerCase()
+							.split('_')
+							.map(
+								(e: string) =>
+									e.charAt(0).toUpperCase() + e.substr(1)
+							)
+							.join('');
+					if ((this as any)[func] !== undefined) {
+						(this as any)[func](data.d);
+					}
+					break;
+				}
+			}
+		};
+	}
+
+	public send(data: object) {
+		return this.ws.send(JSON.stringify(data));
+	}
+
+	private identify() {
+		return this.send({
+			op: OPCodes.IDENTIFY,
+			d: {
+				token: this.token,
+				intents: this.client.opt.intents,
+				properties: {
+					$os: Deno.build.os,
+					$browser: 'Nitro (Deno)',
+					$device: 'Nitro (Deno)',
+				},
+			},
+		});
+	}
+
+	private heartbeat(ms: number) {
+		this.heartbeatInterval = ms;
+		setInterval(() => {
+			this.send({ op: OPCodes.HEARTBEAT, d: null });
+		}, this.heartbeatInterval);
+	}
+
+	private onReady(data: any) {}
+}
